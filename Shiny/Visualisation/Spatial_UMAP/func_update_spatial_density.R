a220a884032b8c181020295f5a7dfd44e28905d2
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2022-12-02 17:10:11 +0100

    repairing density mapping

--- Shiny/Visualisation/Spatial_UMAP/func_update_spatial_density.R
+++ Shiny/Visualisation/Spatial_UMAP/func_update_spatial_density.R
@@ -3,7 +3,6 @@
 ##----------------------------------------------------------------------------##
 
 current_plot_spatial_density <- reactive({
-  
   if(length(input$Plot_display_type_choice) != 1){
     for (n_cell_type in 1:length(input$Plot_display_type_choice)) {
       if(n_cell_type == 1) {
@@ -28,8 +27,8 @@ current_plot_spatial_density <- reactive({
   grid=interp(ic_types$imagecol,ic_types$imagerow,ic_types$sum_IC)
   griddf <- data.frame(x = rep(grid$x, ncol(grid$z)), 
                        y = rep(grid$y, each = nrow(grid$z)), 
-                       z = as.numeric(grid$z))    
-  griddf$z2=ifelse(griddf$z<quantile(griddf$z,na.rm = TRUE,probs = seq(0, 1, 1/10))[2],0,griddf$z)  
+                       z = as.numeric(grid$z))
+  griddf$z2=ifelse(griddf$z<quantile(griddf$z,na.rm = TRUE,probs = seq(0, 1, 1/10))[2],0,griddf$z)
   
   # Create plotly object
   fig <- plot_ly(type = 'scatter',
