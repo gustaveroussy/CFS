48f930dbbdf4000c46e3ce406cd2d238f7b89071
c-thuil <corentin.thuilliez@gustaveroussy.fr>
2023-07-26 03:07:49 +0200

    added informations

---
+++ R/CFS-package.R
@@ -0,0 +1,27 @@
+#' @import shiny
+#' @import shinydashboard
+#' @import shinyFiles
+#' @import shinyWidgets
+#' @import Seurat
+#' @import plotly
+#' @import RColorBrewer
+#' @import scatterpie
+#' @import rclipboard
+#' @import tibble
+#' @import interp
+#' @import stringr
+#' @import scales
+#' @import ggplot2
+#' @import reticulate
+#' @importFrom viridis viridis
+#' @importFrom png readPNG
+#' @importFrom jpeg readJPEG
+#' @importFrom heatmaply heatmaply
+#' @importFrom e1071 kurtosis
+#' @importFrom enrichR listEnrichrDbs setEnrichrSite enrichr
+#' @importFrom pheatmap pheatmap
+#' @importFrom shinyalert shinyalert
+#' @importFrom DT datatable renderDT DTOutput
+#' @importFrom raster as.raster
+NULL
+#> NULL
