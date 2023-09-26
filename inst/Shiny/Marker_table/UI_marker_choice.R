7d243670b9ed39846d733ac9bad146c771790670
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2023-09-26 19:36:19 +0200

    Expanded marker table possibilities for DEG analysis

--- inst/Shiny/Marker_table/UI_marker_choice.R
+++ inst/Shiny/Marker_table/UI_marker_choice.R
@@ -3,7 +3,7 @@ output[["marker_cluster_choice_UI"]] <- renderUI({
   selectInput(
     "marker_cluster_choice",
     label = "Choose cluster marker gene",
-    choices = (as.integer(sort(unique(values$UMAP@meta.data$seurat_clusters)))-1),
+    choices = names(values$marker_gene),
     width = '100%'
   )
 })
--- inst/Shiny/Marker_table/marker_table/UI_Output_save_RDS_content.R
+++ inst/Shiny/Marker_table/marker_table/UI_Output_save_RDS_content.R
@@ -6,40 +6,27 @@
 output[["Marker_table_or_message"]] <- renderDT({
   
   req(values$UMAP)
-  
-  if(is.null(values$marker_gene)){
-    values$marker_gene = marker_table()
-  } else {
-    if(input$overwrite_marker_table){
-      values$marker_gene = marker_table()
-    }
-  }
-  
   req(values$marker_gene)
   
-  DT = Prepare_table_marker(table = values$marker_gene[[(as.integer(input$marker_cluster_choice)+1)]], log = input$Volcano_plot_log_fold_change, pvalue = input$Volcano_plot_p_value)
+  DT = Prepare_table_marker(table = values$marker_gene[[input$marker_cluster_choice]], log = input$Volcano_plot_log_fold_change, pvalue = input$Volcano_plot_p_value)
   
   datatable(DT, options = list(pageLength = 100), class = 'cell-border stripe')#, colnames = c('IC' = 1))
 })
 
 # loading time/button
-marker_table <- eventReactive(input$start_marker, {
+marker_table <- observeEvent(input$start_marker, {
   req(values$UMAP)
-  list_marker = list()
   withProgress(message = 'Preparing table', value = 0, {
-    if(is.null(values$marker_gene)){
-      if (!is.null(values$UMAP)){
-        for (i in 1:length(unique(values$UMAP@meta.data$seurat_clusters))){
-          if(length(rownames(values$UMAP@meta.data)[which(values$UMAP@meta.data$seurat_clusters == (i-1))]) > 3){
-            incProgress((1/as.integer(length(unique(values$UMAP@meta.data$seurat_clusters)))), detail = paste(paste0("Working on cluster ",(i-1))))
-            list_marker[[i]] = FindMarkers(values$UMAP, ident.1 = rownames(values$UMAP@meta.data)[which(values$UMAP@meta.data$seurat_clusters == (i-1))], logfc.threshold = 0.25)
-          }
+    if (!is.null(values$UMAP)){
+      if(is.null(values$marker_gene)){
+        values$marker_gene = list()
+      }
+      for (i in 1:length(input$volcano_plot_clusters_to_compare)){
+        if(nrow(values$UMAP@meta.data[values$UMAP@meta.data$seurat_clusters == (input$volcano_plot_clusters_to_compare[i]),]) > 3){
+          incProgress((1/length(input$volcano_plot_clusters_to_compare)), detail = paste(paste0("Working on cluster ",(input$volcano_plot_clusters_to_compare[i]))))
+          values$marker_gene[[paste0(input$volcano_plot_clusters_to_compare[i],"/", paste(input$volcano_plot_clusters_to_compare[which(input$volcano_plot_clusters_to_compare != input$volcano_plot_clusters_to_compare[i])],collapse=","))]] = FindMarkers(values$UMAP, ident.1 = rownames(values$UMAP@meta.data)[which(values$UMAP@meta.data$seurat_clusters == (input$volcano_plot_clusters_to_compare[i]))], ident.2 = rownames(values$UMAP@meta.data)[which(values$UMAP@meta.data$seurat_clusters %in% (input$volcano_plot_clusters_to_compare[which(input$volcano_plot_clusters_to_compare != input$volcano_plot_clusters_to_compare[i])]))], logfc.threshold = 0.25)
         }
       }
-    } else {
-      incProgress(1, detail = paste("Done"))
-      list_marker = values$marker_gene
     }
   })
-  return(list_marker)
 })
