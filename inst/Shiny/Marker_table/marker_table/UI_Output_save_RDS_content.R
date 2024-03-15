113d1653186de086bb82e347f5016f9a2fe19ac5
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-03-15 15:55:16 +0100

    reworked marker tables

--- inst/Shiny/Marker_table/marker_table/UI_Output_save_RDS_content.R
+++ inst/Shiny/Marker_table/marker_table/UI_Output_save_RDS_content.R
@@ -50,15 +50,45 @@ marker_table <- observeEvent(input$start_marker, {
       if(is.null(values$marker_gene)){
         values$marker_gene = list()
       }
-      for (i in 1:length(input$volcano_plot_clusters_to_compare)){
-        if(nrow(values$data@meta.data[values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] == (input$volcano_plot_clusters_to_compare[i]),]) > 3){
-          incProgress((0.9/length(input$volcano_plot_clusters_to_compare)), detail = paste(paste0("Working on cluster ",(input$volcano_plot_clusters_to_compare[i]))))
-          values$marker_gene[[paste0(input$volcano_plot_clusters_list_to_compare," : ",input$volcano_plot_clusters_to_compare[i],"/", paste(input$volcano_plot_clusters_to_compare[which(input$volcano_plot_clusters_to_compare != input$volcano_plot_clusters_to_compare[i])],collapse=","))]] =
+      
+      
+      
+      if(length(input$volcano_plot_clusters_to_compare) == 1){
+        
+        selected_cluster = input$volcano_plot_clusters_to_compare
+        other_clusters = unique(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare])[!(unique(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare]) %in% selected_cluster)]
+        other_clusters = as.numeric(as.character(other_clusters[order(other_clusters)]))
+        
+
+        if(nrow(values$data@meta.data[values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] == (selected_cluster),]) > 3){
+          
+          incProgress(0.9, detail = paste(paste0("Working on cluster ",selected_cluster)))
+          
+          values$marker_gene[[paste0(input$volcano_plot_clusters_list_to_compare," : ",selected_cluster,"/", paste(other_clusters,collapse=","))]] =
             FindMarkers(values$data,
-                        ident.1 = rownames(values$data@meta.data)[which(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] == (input$volcano_plot_clusters_to_compare[i]))],
-                        ident.2 = rownames(values$data@meta.data)[which(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] %in% (input$volcano_plot_clusters_to_compare[which(input$volcano_plot_clusters_to_compare != input$volcano_plot_clusters_to_compare[i])]))],
+                        ident.1 = rownames(values$data@meta.data)[which(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] == (selected_cluster))],
+                        ident.2 = rownames(values$data@meta.data)[which(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] %in% (other_clusters))],
                         logfc.threshold = 0.25,
                         test.use = input$clustering_method_volcano_plot)
+        }
+
+      } else if (length(input$volcano_plot_clusters_to_compare) > 1) {
+        
+        selected_clusters = input$volcano_plot_clusters_to_compare
+        
+        for (i in selected_clusters){
+          if(nrow(values$data@meta.data[values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] == i,]) > 3){
+            
+            # incProgress((0.9/length(selected_clusters)),
+            #             detail = paste(paste0("Working on cluster ",i)))
+            
+            values$marker_gene[[paste0(input$volcano_plot_clusters_list_to_compare," : ",i,"/", paste(selected_clusters[which(selected_clusters != i)],collapse=","))]] =
+              FindMarkers(values$data,
+                          ident.1 = rownames(values$data@meta.data)[which(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] == i)],
+                          ident.2 = rownames(values$data@meta.data)[which(values$data@meta.data[,input$volcano_plot_clusters_list_to_compare] %in% (selected_clusters[which(selected_clusters != i)]))],
+                          logfc.threshold = 0.25,
+                          test.use = input$clustering_method_volcano_plot)
+          }
         }
       }
     }
