a55fd734efb894d396b4fab8dcd9e668a267c73b
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-05-10 18:07:09 +0200

    added gene IC link table

--- inst/Shiny/ICA_table/UI_gene_table.R
+++ inst/Shiny/ICA_table/UI_gene_table.R
@@ -1,6 +1,34 @@
 output[["ICA_genes_table_UI"]] <- renderUI({
+  req(values$data)
   fluidRow(
-    column(width = 12, offset = 0, style = "padding: 0px;",
+    column(width = 3, offset = 0, style = "padding: 0px;",
+           box(id = "gene_contributive_container",
+               title = tagList(
+                 p("Gene contribution", style = "padding-right: 5px; display: inline"),
+                 actionButton(
+                   inputId = "gene_contribution_info",
+                   label = "info",
+                   icon = NULL,
+                   class = "btn-xs",
+                   title = "Show additional information for this panel.",
+                   style = "margin-right: 3px"
+                 ),
+               ),
+               status = "primary",
+               solidHeader = TRUE,
+               width = 12,
+               height = NULL,
+               collapsible = FALSE,
+               collapsed = FALSE,
+               selectInput(
+                 "ica_gene_table_contributive_to_ic",
+                 "Choose gene",
+                 unique(unlist(lapply(names(values$data@misc$GeneAndStat$Contrib_gene), function(x){return(values$data@misc$GeneAndStat$Contrib_gene[[x]][,"gene"])})))
+               ),
+               DTOutput("out_genes_ic_relation_text")
+           )
+    ),
+    column(width = 9, offset = 0, style = "padding: 0px;",
            box(id = "table_gene_container",
                title = tagList(
                  p("IC gene weights", style = "padding-right: 5px; display: inline"),
