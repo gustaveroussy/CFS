571134125607e775a43f45bcd4aedd50e6e02976
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-05-15 22:24:33 +0200

    updated graph-tab table download

--- inst/Shiny/Interaction_analysis/prepare_IC_distances/func_distance_table_able_Download_handler.R
+++ inst/Shiny/Interaction_analysis/prepare_IC_distances/func_distance_table_able_Download_handler.R
@@ -7,7 +7,7 @@ output$download_distance_tables <- downloadHandler(
     paste("annotation", ".csv", sep = "")
   },
   content = function(file) {
-    table = values$distances[[input$choose_sample_for_distances]][[input$choose_method_for_distances]]
+    table = values$distances[[input$choose_sample_for_distances]][[input$choose_method_for_distances]][["df"]]
     write.csv(table, file, row.names=FALSE)
   }
 )
