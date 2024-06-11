ad0d6255289c5e274cf5dc6ccb10846bb87dc566
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-06-11 16:31:26 +0200

    repaired graph sample selection for genes graphs

--- inst/Shiny/Interaction_analysis/prepare_IC_distances/UI_IC_distances_main_parameters.R
+++ inst/Shiny/Interaction_analysis/prepare_IC_distances/UI_IC_distances_main_parameters.R
@@ -27,7 +27,7 @@ output[["IC_distance_main_parameters_UI"]] <- renderUI({
       ),
       selectInput("choose_sample_for_distances",
                   "Choose sample",
-                  chartr(".", "-", names(values$data@images)),
+                  unique(sapply(strsplit(rownames(values$data@meta.data), "_"),"[[",1)),
                   multiple = FALSE,
                   selectize = TRUE,
                   width = NULL,
