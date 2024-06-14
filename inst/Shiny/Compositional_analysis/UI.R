ee918d800ee672ff0bbaf28814ee1da7a97f5ac8
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-06-14 17:32:01 +0200

    repaired reduction creation for ICs

---
+++ inst/Shiny/Compositional_analysis/UI.R
@@ -0,0 +1,7 @@
+##----------------------------------------------------------------------------##
+## Tab: ICA
+##----------------------------------------------------------------------------##
+tab_compositional_analysis <- tabItem(
+  tabName = "compositional_analysis",
+  uiOutput("Cell_reduction_UI")
+)
