011ed89da1b08b6376ae7e6f1d6a406b5bf63603
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-06-19 15:52:08 +0200

    split preprocessing window into three

--- inst/Shiny/Preprocessing/pre_processing/out_preprocessing.R
+++ inst/Shiny/Preprocessing/pre_processing/out_preprocessing.R
@@ -4,61 +4,71 @@
 
 preprocessing_values <- reactiveValues(button_check = 1, preprocessing_text_display = "<b>Data not available.</b>")
 
-observeEvent(input$preprocessing_action_button, {
+observeEvent(input$preprocessing_normalisation_action_button, {
   req(values$data)
-  if (input$preprocessing_action_button == preprocessing_values$button_check) {
+  
+  i <- if(substr(packageVersion("Seurat"),1,1) == "5"){(Matrix::colSums(values$data@assays$Spatial$counts, na.rm=T) != 0)}else{(Matrix::colSums(values$data@assays$Spatial@counts, na.rm=T) != 0)}
+  row_names_df_to_keep<-colnames(values$data@assays$Spatial)[i]
+  values$data = values$data[, row_names_df_to_keep]
+  
+  values$data = PrepNormData(data=values$data,organism=input$preprocessing_specie_select,variable_features=input$preprocessing_variable_features, min_cells = input$preprocessing_minimum_cell)
+})
+
+observeEvent(input$preprocessing_reduction_action_button, {
+  req(values$data)
+  
+  values$annotation_for_output = list()
+  values$IC_names = NULL
+  values$Stat = NULL
+  values$Annotation = NULL
+  values$marker_gene = NULL
+  values$HD_image = NULL
+  values$HD_image_2 = NULL
+  
+  withProgress(message = 'Reduction', value = 0, {
+    incProgress(0.4, detail = "ICA")
+    values$data=ICASpatial(data=values$data,nics=input$preprocessing_number_of_ICs,maxit=input$preprocessing_maxit,method=input$preprocessing_ICA_function, kurtosis = input$preprocessing_kurtosis, sd = input$preprocessing_sd)
     
-    values$annotation_for_output = list()
-    values$IC_names = NULL
-    values$Stat = NULL
-    values$Annotation = NULL
-    values$marker_gene = NULL
-    values$HD_image = NULL
-    values$HD_image_2 = NULL
+    incProgress(0.3, detail = "Preparing table")
+    if(is.null(values$data@misc$GeneAndStat$kurtosis_value)){
+      values$IC_names = names(values$data@misc$GeneAndStat$Kurtosis_ICs)[values$data@misc$GeneAndStat$Kurtosis_ICs > 3]
+    } else {
+      values$IC_names = names(values$data@misc$GeneAndStat$Kurtosis_ICs)[values$data@misc$GeneAndStat$Kurtosis_ICs > values$data@misc$GeneAndStat$kurtosis_value]
+    }
     
-    i <- if(substr(packageVersion("Seurat"),1,1) == "5"){(Matrix::colSums(values$data@assays$Spatial$counts, na.rm=T) != 0)}else{(Matrix::colSums(values$data@assays$Spatial@counts, na.rm=T) != 0)}
-    row_names_df_to_keep<-colnames(values$data@assays$Spatial)[i]
-    values$data = values$data[, row_names_df_to_keep]
     
-    withProgress(message = 'Pre-processing', value = 0, {
-      incProgress(0.2, detail = "Normalize")
-      values$data = PrepNormData(data=values$data,organism=input$preprocessing_specie_select,variable_features=input$preprocessing_variable_features, min_cells = input$preprocessing_minimum_cell)
-      incProgress(0.4, detail = "Calculating ICs")
-      values$data=ICASpatial(data=values$data,nics=input$preprocessing_number_of_ICs,maxit=input$preprocessing_maxit,method=input$preprocessing_ICA_function, kurtosis = input$preprocessing_kurtosis, sd = input$preprocessing_sd)
-      incProgress(0.3, detail = "Enriching")
-      if(!is.null(input$preprocessing_database)){
-        values$data = Enrich_ICA(data=values$data,dbs=input$preprocessing_database, kurtosis = values$data@misc$GeneAndStat$kurtosis_value, enrich_positive_genes = input$preprocessing_enrichment_positive)
-      }
-      incProgress(0.1, detail = "Done")
-      
-      if(is.null(values$data@misc$GeneAndStat$kurtosis_value)){
-        values$IC_names = names(values$data@misc$GeneAndStat$Kurtosis_ICs)[values$data@misc$GeneAndStat$Kurtosis_ICs > 3]
-      } else {
-        values$IC_names = names(values$data@misc$GeneAndStat$Kurtosis_ICs)[values$data@misc$GeneAndStat$Kurtosis_ICs > values$data@misc$GeneAndStat$kurtosis_value]
-      }
+    row_names = values$IC_names
+    values$data@misc$annotation = matrix(data = "", nrow = length(row_names), ncol = 3)
+    rownames(values$data@misc$annotation) = row_names
+    colnames(values$data@misc$annotation) = c('Use','Type','Annotation')
+    values$data@misc$annotation[,'Use'] = TRUE
     
-      row_names = values$IC_names
-      values$data@misc$annotation = matrix(data = "", nrow = length(row_names), ncol = 3)
-      rownames(values$data@misc$annotation) = row_names
-      colnames(values$data@misc$annotation) = c('Use','Type','Annotation')
-      values$data@misc$annotation[,'Use'] = TRUE
-      
-      values$data@misc$annotation = as.matrix(values$data@misc$annotation)
-      
-      values$Stat = values$data@misc[["GeneAndStat"]]
-      
-      updateSelectizeInput(session, "Ic_list", label = "list of IC",
-                           choices = values$IC_names,
-                           selected = NULL)
-      
-      values$Annotation = values$data@misc$annotation
-      
-      # Get All annotations and their associated ICs
-      associate_signal_with_IC()
-      
-    })
-      preprocessing_values$button_check <- input$preprocessing_action_button + 1
-  }
+    values$data@misc$annotation = as.matrix(values$data@misc$annotation)
+    
+    values$Stat = values$data@misc[["GeneAndStat"]]
+    
+    updateSelectizeInput(session, "Ic_list", label = "list of IC",
+                         choices = values$IC_names,
+                         selected = NULL)
+    
+    values$Annotation = values$data@misc$annotation
+    
+    incProgress(0.2, detail = "Preparing CFS")
+    # Get All annotations and their associated ICs
+    associate_signal_with_IC()
+    
+    incProgress(0.1, detail = "Done")
+  })
+})
+
+observeEvent(input$preprocessing_enrichment_action_button, {
+  req(values$data)
+  withProgress(message = 'Enrichment', value = 0, {
+    incProgress(0.5)
+    if(!is.null(input$preprocessing_database)){
+      values$data = Enrich_ICA(data=values$data,dbs=input$preprocessing_database, kurtosis = values$data@misc$GeneAndStat$kurtosis_value, enrich_positive_genes = input$preprocessing_enrichment_positive)
+    }
+  })
 })
 
 observe({
