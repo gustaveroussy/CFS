57ca6d9bef0937815e5ff3fa9ba1399cf6040761
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-06-20 13:21:31 +0200

    update table in the ica tab

--- inst/Shiny/ICA/Annotation_table/UI_annotation_table.R
+++ inst/Shiny/ICA/Annotation_table/UI_annotation_table.R
@@ -1,6 +1,6 @@
 output[["Annotation_table_UI"]] <- renderDT({
   req(values$Annotation)
-  DT = matrix(values$Annotation[input$IC_choice,], nrow = 1, ncol = 3, dimnames = list(c(input$IC_choice), c('Use','Type','Annotation')))
+  DT = matrix(values$Annotation[input$IC_choice,], nrow = 1, ncol = ncol(values$Annotation), dimnames = list(c(input$IC_choice), colnames(values$Annotation)))
   datatable(DT, editable = list(target = 'cell', disable = list(columns = c(0))), class = 'cell-border stripe', colnames = c('IC' = 1))
 })
 
