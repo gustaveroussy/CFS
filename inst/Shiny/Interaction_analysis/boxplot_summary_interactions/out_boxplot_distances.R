d4d2f5c12de0792a0b77404bede02911e2980707
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-06-26 13:45:04 +0200

    implemented filtering for distances boxplots

--- inst/Shiny/Interaction_analysis/boxplot_summary_interactions/out_boxplot_distances.R
+++ inst/Shiny/Interaction_analysis/boxplot_summary_interactions/out_boxplot_distances.R
@@ -32,29 +32,71 @@ fig_distance_boxplot <- reactive({
   
   df = df[order(df$weight,decreasing = TRUE),]
   
-  agg_df <- aggregate(df$weight, by=list(df$lr), FUN=mean)
-
-  agg_df$x = scale(as.double(agg_df$x))
+  if(!is.null(input$boxplot_interaction_filter_1)){
+    if(input$boxplot_interaction_filter_type == "IC"){
+      
+      df = df[df[,2] %in% input$boxplot_interaction_filter_1,]
+      
+    } else if (input$boxplot_interaction_filter_type %in% colnames(values$Annotation)){
+      
+      IC_filter = rownames(values$Annotation)[values$Annotation[,input$boxplot_interaction_filter_type] %in% input$boxplot_interaction_filter_1]
+      df = df[df[,2] %in% IC_filter,]
+      
+    } else if (input$boxplot_interaction_filter_type == "lr interactions"){
+      
+      df = df[df[,2] %in% input$boxplot_interaction_filter_2,]
+      
+    }
+  }
   
-  # take the values that stand out the most
-  agg_df = agg_df[agg_df$x > input$boxplot_interaction_z_score_filter,]
+  if(!is.null(input$boxplot_interaction_filter_2)){
+    if(input$boxplot_interaction_filter_type == "IC"){
+      
+      df = df[df[,3] %in% input$boxplot_interaction_filter_2,]
+      
+    } else if (input$boxplot_interaction_filter_type %in% colnames(values$Annotation)){
+      
+      IC_filter = rownames(values$Annotation)[values$Annotation[,input$boxplot_interaction_filter_type] %in% input$boxplot_interaction_filter_2]
+      df = df[df[,2] %in% IC_filter,]
+      
+    } else if (input$boxplot_interaction_filter_type == "lr interactions"){
+      
+      df = df[df[,3] %in% input$boxplot_interaction_filter_2,]
+      
+    }
+  }
   
-  df = df[df$lr %in% agg_df$Group.1,]
+  if(nrow(df) > length(samples)){
+    
+    agg_df <- aggregate(df$weight, by=list(df$lr), FUN=mean)
   
-  df$samples = as.character(factor(df$samples, labels = samples))
+    agg_df$x = scale(as.double(agg_df$x))
+    
+    # take the values that stand out the most
+    agg_df = agg_df[agg_df$x > input$boxplot_interaction_z_score_filter,]
+    
+    df = df[df$lr %in% agg_df$Group.1,]
   
-  lvls <- df %>%
-    group_by(lr) %>%
-    summarise(m = median(weight)) %>%
-    arrange(desc(m)) %>%
-    pull(lr)
+  }
   
-  fig <- plot_ly(df, x = ~factor(lr,lvls), y = ~weight, boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
-                 type = "box", text = ~samples,
-                 hovertemplate = paste0("Sample : %{text}<br>",
-                                        "Association : %{x}<br>",
-                                        "Value: %{y}",
-                                        "<extra></extra>"))
-  
-  return(fig)
+  if(nrow(df) > 0){
+    df$samples = as.character(factor(df$samples, labels = samples))
+    
+    lvls <- df %>%
+      group_by(lr) %>%
+      summarise(m = median(weight)) %>%
+      arrange(desc(m)) %>%
+      pull(lr)
+    
+    fig <- plot_ly(df, x = ~factor(lr,lvls), y = ~weight, boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
+                   type = "box", text = ~samples)
+                   # hovertemplate = paste0("Sample : %{text}<br>",
+                   #                        "Association : %{x}<br>",
+                   #                        "Value: %{y}",
+                   #                        "<extra></extra>")
+    
+    return(fig)
+  } else {
+    return(NULL)
+  }
 })
