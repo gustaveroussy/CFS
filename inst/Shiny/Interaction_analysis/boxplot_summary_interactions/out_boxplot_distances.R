a3c3cb3b29b5c40dd210b8d60de4148be50bad1c
c-thuil <Corentin.THUILLIEZ@gustaveroussy.fr>
2024-09-13 11:25:22 +0200

    removed options for boxplots

--- inst/Shiny/Interaction_analysis/boxplot_summary_interactions/out_boxplot_distances.R
+++ inst/Shiny/Interaction_analysis/boxplot_summary_interactions/out_boxplot_distances.R
@@ -46,32 +46,15 @@ fig_distance_boxplot <- reactive({
   if(nrow(df) > length(samples)){
 
     # take the values that stand out the most
-    if(input$boxplot_type_of_filter == "mean values"){
       
-        agg_df <- aggregate(df$weight, by=list(df$lr), FUN=mean)
-        
-        agg_df$x = scale(as.double(agg_df$x))
-        
-        # take the values that stand out the most
-        agg_df = agg_df[agg_df$x > input$boxplot_interaction_z_score_filter,]
-        
-        df = df[df$lr %in% agg_df$Group.1,]
-        
-    } else if(input$boxplot_type_of_filter == "each values"){
-      df = df[df$scaled_weight > input$boxplot_interaction_z_score_filter,]
-    }
+    agg_df <- aggregate(df$weight, by=list(df$lr), FUN=mean)
     
-    if (input$boxplot_type_of_visualisation == "Frequencies") {
-      if(input$choose_distances_to_determine == "ica" & input$choose_distances_to_determine_2 == "ica"){
-        df = df %>% left_join(., Annotation %>% rownames_to_column(var="IC"), by=join_by(!!input$choose_distances_to_determine == IC),suffix = c("", "_1"))
-        df = df %>% left_join(., Annotation %>% rownames_to_column(var="IC"), by=join_by(!!paste0(input$choose_distances_to_determine_2,"_2") == IC),suffix = c("", "_2"))
-      } else if(input$choose_distances_to_determine == "ica"){
-        df = df %>% left_join(., Annotation %>% rownames_to_column(var="IC"), by=join_by(!!input$choose_distances_to_determine == IC),suffix = c("", "_1"))
-      } else if (input$choose_distances_to_determine_2 == "ica"){
-        df = df %>% left_join(., Annotation %>% rownames_to_column(var="IC"), by=join_by(!!input$choose_distances_to_determine_2 == IC),suffix = c("", "_2"))
-      }
-    }
+    agg_df$x = scale(as.double(agg_df$x))
     
+    # take the values that stand out the most
+    agg_df = agg_df[agg_df$x > input$boxplot_interaction_z_score_filter,]
+    
+    df = df[df$lr %in% agg_df$Group.1,]
     
   }
   
@@ -87,33 +70,23 @@ fig_distance_boxplot <- reactive({
       df = df[as.vector(df[,"ica_2"])[[1]] %in% IC_vect,]
     }
     
-    if(input$boxplot_type_of_visualisation == "Absolute value"){
+
       lvls <- df %>%
         group_by(lr) %>%
         summarise(m = median(weight)) %>%
         arrange(desc(m)) %>%
         pull(lr)
       
-      fig <- plot_ly(df, x = ~factor(lr,lvls), y = ~weight, boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
-                     type = "box", text = ~samples)
-      
-    } else if (input$boxplot_type_of_visualisation == "Frequencies") {
-      # determines frequencies
-      if(input$choose_distances_to_determine == input$choose_distances_to_determine_2){
-        df = df %>% group_by_at(c(input$choose_distances_to_determine, paste0(input$choose_distances_to_determine_2,"_2"),"samples")) %>% summarise(n=n()) %>% mutate(freq = n/sum(n))
-        
-        fig <- plot_ly(df, x = as.vector(df[,input$choose_distances_to_determine])[[1]], y = ~freq, color = as.vector(df[,paste0(input$choose_distances_to_determine_2,"_2")])[[1]], boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
-                       type = "box", text = ~samples)
-      } else {
-        df = df %>% group_by_at(c(input$choose_distances_to_determine, input$choose_distances_to_determine_2,"samples")) %>% summarise(n=n()) %>% mutate(freq = n/sum(n))
-        fig <- plot_ly(df, x = as.vector(df[,input$choose_distances_to_determine])[[1]], y = ~freq, color = as.vector(df[,input$choose_distances_to_determine_2])[[1]], boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
-                       type = "box", text = ~samples)
-      }
-      
-      fig <- fig %>% layout(boxmode = "group")
-      
-    }
+    fig <- plot_ly(df, x = ~factor(lr,lvls), y = ~weight, boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
+                   type = "box", text = ~samples)
     
+
+    #   df = df %>% group_by_at(c(input$choose_distances_to_determine, input$choose_distances_to_determine_2,"samples")) %>% summarise(n=n()) %>% mutate(freq = n/sum(n))
+    #   fig <- plot_ly(df, x = as.vector(df[,input$choose_distances_to_determine])[[1]], y = ~freq, color = as.vector(df[,input$choose_distances_to_determine_2])[[1]], boxmean = TRUE, boxpoints = input$boxplot_interaction_boxploint_type,
+    #                  type = "box", text = ~samples)
+    # 
+    # 
+    # fig <- fig %>% layout(boxmode = "group")
     
     return(fig)
   } else {
